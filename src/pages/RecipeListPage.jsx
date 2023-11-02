375b6baea944782cba23a702dd24738c45eb2d18
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-02 20:19:39 +0100

    Create recipes.map

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,13 +1,22 @@
 import { Center, Heading } from '@chakra-ui/react';
+import { RecipeCard } from '../components/RecipeCard';
 import { data } from '../utils/data';
 
-export const RecipeListPage = () => {
+export const RecipeListPage = (clickFn) => {
   // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
+  // console.log(data.hits[0].recipe.label);
+  // console.log(data.hits[2].recipe.label);
+  // console.log(data.hits);
 
+ const recipes = data.hits
+ 
   return (
     <Center h="100vh" flexDir="column">
       <Heading>Your Recipe App</Heading>
+        {recipes.map((recipe) => (
+          <RecipeCard clickFn={clickFn} recipe={recipe} key={recipe.recipe.label} />
+  ))}
     </Center>
   );
 };
+  
