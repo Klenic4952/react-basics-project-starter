5e7049e35982cd9746ae3d10f66bfb82d807090f
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-02 10:05:18 +0100

    Add RecipePage

---
+++ src/pages/RecipePage.jsx
@@ -0,0 +1,11 @@
+import { Center } from "@chakra-ui/react";
+//import { data } from "../utils/data";
+
+export const RecipePage = () => {
+    return (
+        <Center>
+            Showing recipe in the future
+        </Center>
+    )
+
+}
