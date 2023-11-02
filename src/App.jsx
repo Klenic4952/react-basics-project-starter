2f0db904d50b68d34731b5f26e73021835150d57
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-02 10:06:15 +0100

    Add clickFn to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
 import { RecipeListPage } from './pages/RecipeListPage';
+import { RecipePage } from './pages/RecipePage';
+
 
 export const App = () => {
   const [selectedRecipe, setSelectedRecipe] = useState();
-  
-  return <RecipeListPage />;
+
+  return (
+    <div>
+      {selectedRecipe ? (
+          <RecipePage recipe={selectedRecipe} clickFn={setSelectedRecipe}/>
+      ) : (
+          <RecipeListPage clickFn={setSelectedRecipe}/>
+      )}
+   </div>
+  );
 };
