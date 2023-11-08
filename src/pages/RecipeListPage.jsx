7b639e0bc05771981c2a9fba8709c05e292184bb
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-08 17:47:31 +0100

    Add styles and map

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,22 +1,34 @@
-import { Center, Heading } from '@chakra-ui/react';
+import { Box, Container, Heading, SimpleGrid } from '@chakra-ui/react';
 import { RecipeCard } from '../components/RecipeCard';
 import { data } from '../utils/data';
 
-export const RecipeListPage = (clickFn) => {
+export const RecipeListPage = ({clickFn}) => {
   // You can play around with the console log, but ultimately remove it once you are done
   // console.log(data.hits[0].recipe.label);
   // console.log(data.hits[2].recipe.label);
-  // console.log(data.hits);
+  
 
  const recipes = data.hits
+ //console.log(data.hits);
  
   return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
-        {recipes.map((recipe) => (
-          <RecipeCard clickFn={clickFn} recipe={recipe} key={recipe.recipe.label} />
-  ))}
-    </Center>
+    <div>
+      <Box display="flex" justifyContent="center">
+        <Heading margin="20">Your Recipe App</Heading>
+      </Box>
+      <Container maxW="80%">
+        <SimpleGrid spacing="5" minChildWidth="300px">
+        
+          {recipes.map((recipe) => (
+              <RecipeCard
+                clickFn={clickFn} 
+                recipe={recipe} 
+                key={recipe.recipe.url}          
+              />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </div>
   );
 };
   
