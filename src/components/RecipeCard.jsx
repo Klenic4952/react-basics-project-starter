8d38859490a2dedd8665f30ced7791acc8fd4352
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-08 17:51:51 +0100

    Work on Card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,21 +1,46 @@
-import { Card, CardBody, Image } from "@chakra-ui/react"
+import { Box, Card, CardBody, Center, Flex, HStack, Heading, Image, Stack, Text, VStack } from "@chakra-ui/react"
+import {Tag} from "./ui/Tag";
 
 export const RecipeCard = ({recipe, clickFn}) => {
 
 
+
     return (
         <Card
+            maxW="sm"
+            borderRadius="xl"
+            cursor="pointer"
             onClick={() => clickFn(recipe)}
         >
-            <CardBody>
-                <Image
-                    w="sm"
-                    h={64}
-                    src={recipe.recipe.image}
-                    
-                >
-
-                </Image>
+            <CardBody padding="0" align="center">
+                <Image            
+                    h="200px"
+                    w="full"
+                    objectFit="cover"
+                    borderTopRadius="xl"           
+                    src={recipe.recipe.image} 
+                />
+            
+            < Stack m={[1, 6]} spacing="-0.5" >
+                <Text fontSize="14px" color="grey" fontWeight="medium" textTransform="uppercase">{recipe.recipe.mealType}</Text>
+                <Heading size="md" fontWeight="medium">{recipe.recipe.label}</Heading>
+                
+                <HStack mt={3} spacing={3} justify="center">
+                {recipe.recipe.healthLabels.filter((label) => 
+                    label === "Vegetarian" || label ==="Vegan").map((label) => (
+                    <Tag key={label} bgColor="purple.100">{label}</Tag>
+                    ))}   
+                </HStack>
+                
+                <HStack mt={3} spacing={3} justify="center">
+                {recipe.recipe.dietLabels.map((label) => {
+                    return (
+                        <Tag key={label}>{label}</Tag>
+                    );
+                })}
+                </HStack>
+                           
+            </Stack>         
             </CardBody>  
         </Card>
             
