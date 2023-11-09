e89428d096d5e2dddbddd45ef014b1d3e3fe88ec
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-09 20:26:13 +0100

    Edit RecipePage

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,11 +1,167 @@
-import { Center } from "@chakra-ui/react";
-//import { data } from "../utils/data";
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { Tag } from "../components/ui/Tag";
 
-export const RecipePage = () => {
-    return (
-        <Center>
-            Showing recipe in the future
-        </Center>
-    )
+export const RecipePage = ({ recipe }) => {
+  return (
+    <Container h="100vh" mt="5" flexDirection="column" maxW="60%">
+      <Card>
+        <CardBody padding="0">
+          <Image
+            h="300px"
+            w="full"
+            mt="10"
+            objectFit="cover"
+            src={recipe.recipe.image}
+          />
 
-}
+          <SimpleGrid spacing="5" m="7" minChildWidth="50px">
+            <Box>
+              <Text
+                fontSize="14px"
+                color="grey"
+                fontWeight="medium"
+                textTransform="uppercase"
+              >
+                {recipe.recipe.mealType}
+              </Text>
+              <Heading size="md" mt="1" fontWeight="medium">
+                {recipe.recipe.label}
+              </Heading>
+              <Flex gap="2" mt="3">
+                <Text>Total cooking time:</Text>
+                <Text fontWeight="medium">
+                  {recipe.recipe.totalTime} minutes
+                </Text>
+              </Flex>
+              <Flex gap="2" mt=".5">
+                <Text>Servings:</Text>
+                <Text fontWeight="medium">{recipe.recipe.yield}</Text>
+              </Flex>
+              <Box mt="2">
+                <Text fontSize="17px" fontWeight="semibold">
+                  Ingredients:
+                </Text>
+                {recipe.recipe.ingredientLines.map((ingredient) => {
+                  return (
+                    <Text lineHeight="2" key={ingredient}>
+                      {ingredient}
+                    </Text>
+                  );
+                })}
+              </Box>
+            </Box>
+            <Box>
+              <Text>Health Labels:</Text>
+              <Box>
+                {recipe.recipe.healthLabels.map((label) => {
+                  return (
+                    <Tag key={label} bgColor="purple.100" mr="1" mb="1">
+                      {label}
+                    </Tag>
+                  );
+                })}
+              </Box>
+              <Text mt="2">Diet:</Text>
+              <Box>
+                {recipe.recipe.dietLabels.map((diet) => {
+                  return (
+                    <Tag key={diet} bgColor="green.100" mr="1" mb="1">
+                      {diet}
+                    </Tag>
+                  );
+                })}
+              </Box>
+              <Text>Cautions:</Text>
+              <Box>
+                {recipe.recipe.cautions.map((label) => {
+                  return (
+                    <Tag key={label} bgColor="#FCC3B2" mr="1" mb="1">
+                      {label}
+                    </Tag>
+                  );
+                })}
+              </Box>
+              <Text fontWeight="semibold" mt="2" mb="2">
+                Total nutriënts:
+              </Text>
+              <Flex
+                flexDir="row"
+                wrap="wrap"
+                justify="flex-start"
+                columnGap="3.5"
+                mb="15px"
+              >
+                <Flex flexDir="column" justify="space-evenly">
+                  <Flex fontSize="14px">
+                    {Math.round(
+                      recipe.recipe.totalNutrients.ENERC_KCAL.quantity
+                    )}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    CALORIES
+                  </Text>
+                </Flex>
+                <Flex flexDir="column">
+                  <Flex fontSize="14px">
+                    {Math.round(recipe.recipe.totalNutrients.CHOCDF.quantity)}
+                    {recipe.recipe.totalNutrients.CHOCDF.unit}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    {recipe.recipe.totalNutrients.CHOCDF.label.toUpperCase()}
+                  </Text>
+                </Flex>
+
+                <Flex flexDir="column">
+                  <Flex fontSize="14px">
+                    {Math.round(recipe.recipe.totalNutrients.PROCNT.quantity)}
+                    {recipe.recipe.totalNutrients.PROCNT.unit}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    {recipe.recipe.totalNutrients.PROCNT.label.toUpperCase()}
+                  </Text>
+                </Flex>
+                <Flex flexDir="column">
+                  <Flex fontSize="14px">
+                    {Math.round(recipe.recipe.totalNutrients.FAT.quantity)}
+                    {recipe.recipe.totalNutrients.FAT.unit}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    {recipe.recipe.totalNutrients.FAT.label.toUpperCase()}
+                  </Text>
+                </Flex>
+                <Flex flexDir="column">
+                  <Flex fontSize="14px">
+                    {Math.round(recipe.recipe.totalNutrients.CHOLE.quantity)}
+                    {recipe.recipe.totalNutrients.CHOLE.unit}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    {recipe.recipe.totalNutrients.CHOLE.label.toUpperCase()}
+                  </Text>
+                </Flex>
+                <Flex flexDir="column">
+                  <Flex fontSize="14px">
+                    {Math.round(recipe.recipe.totalNutrients.NA.quantity)}
+                    {recipe.recipe.totalNutrients.NA.unit}
+                  </Flex>
+                  <Text fontWeight="semibold" fontSize="14px">
+                    {recipe.recipe.totalNutrients.NA.label.toUpperCase()}
+                  </Text>
+                </Flex>
+              </Flex>
+            </Box>
+          </SimpleGrid>
+        </CardBody>
+      </Card>
+    </Container>
+  );
+};
