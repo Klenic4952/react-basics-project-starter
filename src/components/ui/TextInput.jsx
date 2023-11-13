f6ff7f256a8966e54f2e5637492d62f3820a21d8
NICOLIEN KLEIN <nicolienklein@live.nl>
2023-11-13 17:07:46 +0100

    Add TextInput

---
+++ src/components/ui/TextInput.jsx
@@ -0,0 +1,14 @@
+import { Input } from "@chakra-ui/react";
+
+export const TextInput = ({ changeFn, ...props }) => {
+  return (
+    <Input
+      variant="outline"
+      
+      type="text"
+      color="black"
+      onChange={changeFn}
+      {...props}
+    ></Input>
+  );
+};
